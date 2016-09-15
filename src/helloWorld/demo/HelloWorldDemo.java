ffb5a58f10d1cdf51d19a7a991f1efd02d4a0ee4
naz <naz@ubuntu>
2016-09-15 10:25:26 +0100

    First commit

---
+++ src/helloWorld/demo/HelloWorldDemo.java
@@ -0,0 +1,10 @@
+package helloWorld.demo;
+
+public class HelloWorldDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
