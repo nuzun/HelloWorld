90356f5b49f5c1b8482430936f0527402eb7a7d3
naz <naz@ubuntu>
2016-09-15 11:46:20 +0100

    version 2 hey

--- src/helloWorld/demo/HelloWorldDemo.java
+++ src/helloWorld/demo/HelloWorldDemo.java
@@ -5,6 +5,7 @@ public class HelloWorldDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("Hello World hey!");
 	}
 
 }
